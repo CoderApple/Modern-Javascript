5edb23181dd0d7bd4a077b50a8f3cb46d2eb8e64
akshatsinghdelhi@gmail.com <akshatsinghdelhi@gmail.com>
2022-01-22 13:35:18 +0530

    Logs and variables

---
+++ 1. Syntax, Basics and Types/1.1 Basics/sandbox.js
@@ -0,0 +1,20 @@
+// alert('hello world');
+// console.log(1);
+// console.log(2);
+
+let age = 25;
+let year = 2022;
+
+// log things to console
+console.log(age, year);
+
+
+age = 30;
+console.log(age);
+
+const point = 100;
+// point = 50;
+console.log(point);
+
+var score = 75;  // old way
+console.log(score);
