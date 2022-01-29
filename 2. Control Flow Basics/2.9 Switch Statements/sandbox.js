4ff639998637a8f9b24f7ee2fc14865ca5d42cb6
akshatsinghdelhi@gmail.com <akshatsinghdelhi@gmail.com>
2022-01-29 21:54:38 +0530

    Switch statement

---
+++ 2. Control Flow Basics/2.9 Switch Statements/sandbox.js
@@ -0,0 +1,39 @@
+// switch statement
+const grade = 'D';
+
+switch (grade) {
+    case 'A':
+        console.log("You got an A!");
+        break;
+    case 'B':
+        console.log("You got an B!");
+        break;
+    case 'C':
+        console.log("You got an C!");
+        break;
+    case 'D':
+        console.log("You got an D!");
+        break;
+    case 'E':
+        console.log("You got an E!");
+        break;
+
+    default:
+        console.log("Invalid grade");
+        break;
+}
+
+// if else statement
+// if (grade == 'A') {
+    
+// } else if(grade == 'B'){
+
+// } else if(grade == 'C'){
+
+// } else if(grade == 'D'){
+
+// } else if(grade == 'E'){
+
+// } else {
+
+// }
