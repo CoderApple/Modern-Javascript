6599440f0aa50ff74661b6b16bf40e1216399eaf
akshatsinghdelhi@gmail.com <akshatsinghdelhi@gmail.com>
2022-01-29 07:15:10 +0530

    Using if statements

---
+++ 2. Control Flow Basics/2.4 If statements/sandbox.js
@@ -0,0 +1,18 @@
+// if statements
+
+const age = 20;
+
+if (age > 18) {
+    console.log("you are an adult");
+}
+
+const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
+
+if (ninjas.length > 3) {
+    console.log("that's a lot of ninjas");
+}
+
+const password = 'password';
+if (password.length >= 8) {
+    console.log("that password is long enough!")
+}
