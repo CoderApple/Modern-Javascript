90dc885e12036d1ffc2ecee4afda9ca58d3b9ed7
akshatsinghdelhi@gmail.com <akshatsinghdelhi@gmail.com>
2022-01-31 20:33:26 +0530

    returning values through return keyword

---
+++ 3. Functions and Methods/3.3 Returning Values/sandbox.js
@@ -0,0 +1,10 @@
+// returning values
+
+const CalcArea = function(radius) {
+    // let area = 3.14 * radius ** 2;
+    // return area;
+    return 3.14 * radius ** 2;
+};
+
+const area = CalcArea(5);
+console.log(area);
