ced4567088afb8f9da34274eca81bb2e9151c658
akshatsinghdelhi@gmail.com <akshatsinghdelhi@gmail.com>
2022-02-08 21:08:09 +0530

    Methods are synonyms of Functions

---
+++ 3. Functions and Methods/3.5 Functions vs Methods/sandbox.js
@@ -0,0 +1,13 @@
+const name = 'shaun';
+
+// Functions
+
+const greet = () => 'hello';
+
+let resultOne = greet();
+console.log(resultOne);
+
+// nethods
+
+let resultTwo = name.toUpperCase();
+console.log(resultTwo);
