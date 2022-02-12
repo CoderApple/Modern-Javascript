09df542b43331d6086074d4fd9814d7d098cef27
akshatsinghdelhi@gmail.com <akshatsinghdelhi@gmail.com>
2022-02-12 20:48:38 +0530

    adding methods in objects

---
+++ 4. Object Literals/4.2 Adding Methods/sandbox.js
@@ -0,0 +1,23 @@
+let user = {
+    name: 'crystal', 
+    age: 30,
+    email: 'crystal@thenetninja.co.uk',
+    location: 'berlin',
+    blogs: ['why mac & cheese rules', '10 things to make with marmite'],
+    login: function(){
+        console.log('the user logged in');
+    },
+    logout: function(){
+        console.log('the user logged out');
+    },
+    logBlogs: function(){
+
+    }
+};
+
+user.login();
+user.logout();
+
+// similar to methods in strings
+const name = 'mario';
+console.log(name.toUpperCase());
