68ecbd61ff8828f86392bde38d8b36d9bfa17569
akshatsinghdelhi@gmail.com <akshatsinghdelhi@gmail.com>
2022-02-14 16:41:13 +0530

    Changing HTML Attributes

---
+++ 5. The Document Object Model/5.4 Getting & Setting Attributes/sandbox.js
@@ -0,0 +1,10 @@
+const link = document.querySelector('a');
+
+console.log(link.getAttribute('href'));
+link.setAttribute('href', 'https://www.thenetninja.co.uk');
+link.innerHTML = 'The Net Ninja Website';
+
+const msg = document.querySelector('p');
+console.log(msg.getAttribute('class'));
+msg.setAttribute('class', "success");
+msg.setAttribute('style', 'color: green;');
