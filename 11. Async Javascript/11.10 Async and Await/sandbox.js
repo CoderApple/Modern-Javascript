fba7e41f6371581e1929e7a43ad93a79ebdd8d3b
Akshat Singh <95408545+CoderApple@users.noreply.github.com>
2022-04-10 16:49:24 +0530

    Async functions

---
+++ 11. Async Javascript/11.10 Async and Await/sandbox.js
@@ -0,0 +1,23 @@
+// async & await
+
+const getTodos = async () =>{
+
+
+    const response = await fetch('todos/luigi.json');
+    const data = await response.json();
+    
+    return data;
+};
+
+getTodos()
+    .then(data => console.log('resolved:', data));
+
+
+// fetch('todos/luigi.json').then((response) =>{
+//     console.log('resolved', response);
+//     return response.json();
+// }).then(data => {
+//     console.log(data);
+// }).catch((err) => {
+//     console.log('rejected', err);
+// });
