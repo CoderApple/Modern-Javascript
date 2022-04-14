88afed059f701dd43baa3c0bc23a65bf7350c0b1
Akshat Singh <95408545+CoderApple@users.noreply.github.com>
2022-04-14 19:14:20 +0530

    Taking a recap

---
+++ 14. Object Oriented Javascript/14.1 Object Literal Recap/sandbox.js
@@ -0,0 +1,13 @@
+const userOne = {
+    username: 'ryu',
+    email: 'ryu@thenetninja.co.uk',
+    login(){
+        console.log('the user logged in');
+    },
+    logout(){
+        console.log('the user logged out');
+    }
+};
+
+console.log(userOne.email, userOne.username);
+userOne.login();
