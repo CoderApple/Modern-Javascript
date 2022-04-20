ff74fe5b7932769fbbe82ed5b817f387368293d0
Akshat Singh <95408545+CoderApple@users.noreply.github.com>
2022-04-20 22:39:01 +0530

    Tabs

--- 18. Project - UI Library/scripts/sandbox.js
+++ 18. Project - UI Library/scripts/sandbox.js
@@ -3,9 +3,14 @@ const tooltip = new Tooltip(document.querySelector('.tooltip'));
 
 tooltip.init();
 
+// create a dropdown
 const dropdowns = document.querySelectorAll('.dropdown');
 
 dropdowns.forEach(dropdown =>{
     const instance = new Dropdown(dropdown);
     instance.init();
 })
+
+// create tabs
+const tabs = new Tabs(document.querySelector('.tabs'));
+tabs.init();
---
+++ 18. Project - UI Library/scripts/tabs.js
@@ -0,0 +1,30 @@
+class Tabs{
+    constructor(container){
+        this.container = container;
+        this.tabs = container.querySelectorAll('.trigger');
+    }
+    init(){
+        this.tabs.forEach((tab) =>{
+            tab.addEventListener('click', (e) =>{
+                this.toggleTabs(e);
+                this.toggleContent(e);
+            });
+        });
+    }
+    toggleTabs(e){
+        // remove current active classes
+        this.tabs.forEach(tab => tab.classList.remove('tabactive'));
+        // add new active class to clicked tab
+        e.target.classList.add('tabactive');
+    }
+    toggleContent(e){
+        // remove current active classes from content
+        this.container.querySelectorAll('.content').forEach(item => {
+            item.classList.remove('tabactive');
+        })
+        // add new active class to content
+        const selector = e.target.getAttribute('data-target');
+        const content = this.container.querySelector(selector);
+        content.classList.add('tabactive');
+    }
+}
